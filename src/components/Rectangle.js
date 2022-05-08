346b6cd736de87b5fa7366e049a12d55a59e7c35
MakzaR <makzarchukov@gmail.com>
2022-05-08 22:15:50 +0500

    Added rectangle add function

---
+++ src/components/Rectangle.js
@@ -0,0 +1,59 @@
+import React, {useEffect, useRef} from "react";
+import {Rect, Transformer} from "react-konva";
+
+export default function Rectangle({shapeProps, isSelected, onSelect, onChange}) {
+    const shapeRef = useRef();
+    const trRef = useRef();
+
+    useEffect(() => {
+        if (isSelected) {
+            trRef.current.nodes([shapeRef.current]);
+            trRef.current.getLayer().batchDraw();
+        }
+    }, [isSelected]);
+
+    return (
+        <React.Fragment>
+            <Rect
+                onClick={onSelect}
+                ref={shapeRef}
+                {...shapeProps}
+                draggable
+                onDragEnd={(e) => {
+                    onChange({
+                        ...shapeProps,
+                        x: e.target.x(),
+                        y: e.target.y(),
+                    })
+                }}
+                onTransformEnd={(e) => {
+                    const node = shapeRef.current;
+                    const scaleX = node.scaleX();
+                    const scaleY = node.scaleY();
+
+                    node.scaleX(1);
+                    node.scaleY(1);
+
+                    onChange({
+                        ...shapeProps,
+                        x: node.x(),
+                        y: node.y(),
+                        width: Math.max(5, node.width() * scaleX),
+                        height: Math.max(node.height() * scaleY),
+                    })
+                }}
+            />
+            {isSelected && (
+                <Transformer
+                    ref={trRef}
+                    boundBoxFunc={(oldBox, newBox) => {
+                        if (newBox.width < 5 || newBox.height < 5) {
+                            return oldBox;
+                        }
+                        return newBox;
+                    }}
+                />
+            )}
+        </React.Fragment>
+    );
+}
--- src/components/Whiteboard.js
+++ src/components/Whiteboard.js
@@ -1,13 +1,17 @@
 import React, {useRef, useState} from 'react';
 import {Layer, Rect, Stage} from 'react-konva';
-import Circ from "./Circle";
+import Circ from './Circle';
+import Rectangle from "./Rectangle";
+
+import styles from './Whiteboard.module.css'
 
 const SCALE_BY = 1.2;
-const SCALE_MAX = 10;
+const SCALE_MAX = 5;
 const SCALE_MIN = 0.5;
 
 export default function Whiteboard() {
     const [circles, setCircles] = useState([]);
+    const [rectangles, setRectangles] = useState([]);
     const [selectedId, selectShape] = useState(null);
 
     const stageEl = useRef();
@@ -31,9 +35,9 @@ export default function Whiteboard() {
                 y: (pointerY - stage.y()) / oldScale,
             };
             const newScale = event.evt.deltaY > 0 ? oldScale * SCALE_BY : oldScale / SCALE_BY;
-            console.log(newScale);
+            console.log(newScale)
             if (newScale < SCALE_MAX && newScale > SCALE_MIN) {
-                stage.scale({ x: newScale, y: newScale });
+                stage.scale({x: newScale, y: newScale});
                 const newPos = {
                     x: pointerX - mousePointTo.x * newScale,
                     y: pointerY - mousePointTo.y * newScale,
@@ -57,42 +61,77 @@ export default function Whiteboard() {
         setCircles(newCircles);
     };
 
+    const addRectangle = () => {
+        const rect = {
+            x: 100,
+            y: 100,
+            width: 100,
+            height: 100,
+            stroke: "black",
+            id: `rect${rectangles.length + 1}`,
+        };
+        const newRects = rectangles.concat([rect]);
+        setRectangles(newRects);
+    }
+
     return (
         <div>
-            <button onClick={addCircle}>Add circle</button>
-            <Stage
-                ref={stageEl}
-                width={window.innerWidth}
-                height={window.innerHeight}
-                draggable
-                onMouseDown={checkDeselect}
-                onWheel={zoomStage}
-            >
-                <Layer ref={layerEl}>
-                    <Rect
-                        width={window.innerWidth}
-                        height={window.innerHeight}
-                        fill={'grey'}
-                    />
-                    {circles.map((circle, i) => {
-                        return (
-                            <Circ
-                                key={i}
-                                shapeProps={circle}
-                                isSelected={circle.id === selectedId}
-                                onSelect={() => {
-                                    selectShape(circle.id);
-                                }}
-                                onChange={(newAttrs) => {
-                                    const newCircles = circles.slice();
-                                    newCircles[i] = newAttrs;
-                                    setCircles(newCircles);
-                                }}
-                            />
-                        )
-                    })}
-                </Layer>
-            </Stage>
+            <button className={styles.circle_button} onClick={addCircle}>Add circle</button>
+            <button className={styles.rect_button} onClick={addRectangle}>Add rectangle</button>
+            <div>
+                <Stage
+                    className={styles.whiteboard}
+                    ref={stageEl}
+                    width={3000}
+                    height={1500}
+                    draggable
+                    onMouseDown={checkDeselect}
+                    onWheel={zoomStage}
+                >
+                    <Layer ref={layerEl}>
+                        <Rect
+                            width={3000}
+                            height={1500}
+                            fill={'white'}
+                            id={'background'}
+                        />
+                        {circles.map((circle, i) => {
+                            return (
+                                <Circ
+                                    key={i}
+                                    shapeProps={circle}
+                                    isSelected={circle.id === selectedId}
+                                    onSelect={() => {
+                                        selectShape(circle.id);
+                                    }}
+                                    onChange={(newAttrs) => {
+                                        const newCircles = circles.slice();
+                                        newCircles[i] = newAttrs;
+                                        setCircles(newCircles);
+                                    }}
+                                />
+                            )
+                        })}
+                        {rectangles.map((rectangle, i) => {
+                            return (
+                                <Rectangle
+                                    key={i}
+                                    shapeProps={rectangle}
+                                    isSelected={rectangle.id === selectedId}
+                                    onSelect={() => {
+                                        selectShape(rectangle.id);
+                                    }}
+                                    onChange={(newAttrs) => {
+                                        const newRects = rectangles.slice();
+                                        newRects[i] = newAttrs;
+                                        setRectangles(newRects);
+                                    }}
+                                />
+                            )
+                        })}
+                    </Layer>
+                </Stage>
+            </div>
         </div>
     )
 }
